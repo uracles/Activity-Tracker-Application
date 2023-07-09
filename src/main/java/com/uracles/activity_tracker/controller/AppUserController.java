784a8af04cd42b18734fd83cbf1b1d59658dd6c4
Miracle Shaibu <120692848+uracles@users.noreply.github.com>
2023-07-09 23:40:23 +0100

    almost done

--- src/main/java/com/uracles/activity_tracker/controller/AppUserController.java
+++ src/main/java/com/uracles/activity_tracker/controller/AppUserController.java
@@ -1,13 +1,10 @@
 package com.uracles.activity_tracker.controller;
 
-import com.uracles.activity_tracker.dto.request.AppUserLoginRequest;
-import com.uracles.activity_tracker.dto.request.AppUserSignUpRequest;
-import com.uracles.activity_tracker.dto.response.AppUserLoginResponse;
+import com.uracles.activity_tracker.dto.request.AppUserRequest;
 import com.uracles.activity_tracker.service.AppUserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
@@ -21,28 +18,29 @@ public class AppUserController {
     private final AppUserService appUserService;
 
     @PostMapping("/signup")
-    public ResponseEntity<?> registerUser(@RequestBody @Valid AppUserSignUpRequest request){
+    public ResponseEntity<?> registerUser(@RequestBody @Valid AppUserRequest request){
         var response = appUserService.registerUser(request);
         return ResponseEntity.ok(response);
     }
 
-//    @PostMapping("/login")
-//    public ResponseEntity<?> loginUser (@RequestBody AppUserLoginRequest appUserLoginRequest, HttpServletRequest request){
-//        var loginResponse = appUserService.loginUser(appUserLoginRequest.getEmail(), appUserLoginRequest.getPassword());
-//        return ResponseEntity.ok(loginResponse);
-//    }
+    @PostMapping("/login")
+    public ResponseEntity<?> loginUser (@RequestBody @Valid AppUserRequest appUserLoginRequest){
+        var loginResponse = appUserService.loginUser(appUserLoginRequest.getEmail(), appUserLoginRequest.getPassword());
+        return ResponseEntity.ok(loginResponse);
+    }
 
 
-//    @DeleteMapping("/deleteUser")
-//    public ResponseEntity<String> deleteUser(){
-//        appUserService.deleteAppUser();
-//        return new ResponseEntity<>("user deleted successfully", HttpStatus.NO_CONTENT);
-//    }
+    @DeleteMapping("/deleteUser")
+    public ResponseEntity<?> deleteUser(){
+        appUserService.deleteUser();
+        return new ResponseEntity<>("user deleted successfully", HttpStatus.NO_CONTENT);
+    }
 
 
-
-    // @GetMapping("/{id}")
-//    public ResponseEntity<AppUserResponseDto> getAppUser(@PathVariable Long id){
-//        return new ResponseEntity<>(appUserService.getAppUser(id),HttpStatus.ACCEPTED);
-//    }
+     @GetMapping("/{id}")
+    public ResponseEntity<?> findAppUser(@PathVariable Long id){
+         var response = appUserService.viewAppUser(id);
+//        return new ResponseEntity<>(appUserService.viewAppUser(id),HttpStatus.ACCEPTED);
+         return ResponseEntity.ok(response);
+    }
 }
