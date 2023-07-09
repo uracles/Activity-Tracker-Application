3d8c5965e877c3207386702e9e36e0c6e08295a2
Miracle Shaibu <120692848+uracles@users.noreply.github.com>
2023-07-09 23:37:49 +0100

    reworked on the mapper

--- src/main/java/com/uracles/activity_tracker/utils/Mapper.java
+++ src/main/java/com/uracles/activity_tracker/utils/Mapper.java
@@ -1,22 +1,25 @@
 package com.uracles.activity_tracker.utils;
 
-import com.uracles.activity_tracker.dto.response.AppUserLoginResponse;
-import com.uracles.activity_tracker.dto.response.AppUserSignUpResponse;
+import com.uracles.activity_tracker.dto.response.AppUserResponse;
 import com.uracles.activity_tracker.dto.response.TaskResponse;
 import com.uracles.activity_tracker.entities.AppUser;
 import com.uracles.activity_tracker.entities.Task;
 
 public class Mapper {
 //    public static AppUserLoginResponse createResponseDto(AppUser appUser) {
-        public static AppUserSignUpResponse createResponseDto(AppUser appUser) {
+        public static AppUserResponse createResponseDto(AppUser appUser) {
 
 //        return AppUserLoginResponse.builder()
-        return AppUserSignUpResponse
+        return AppUserResponse
                 .builder().id(appUser.getId()).email(appUser.getEmail()).name(appUser.getName()).build();
     }
 
-    public static TaskResponse generateTaskResponse(Task task){
+    public static TaskResponse convertToTaskResponse(Task task){
         return TaskResponse.builder()
-                .taskId(task.getId()).title(task.getTitle()).description(task.getDescription()).taskStatus(task.getTaskStatus()).build();
+                .id(task.getId())
+                .title(task.getTitle())
+                .description(task.getDescription())
+//                .taskStatus(task.getTaskStatus())
+                .build();
     }
 }
